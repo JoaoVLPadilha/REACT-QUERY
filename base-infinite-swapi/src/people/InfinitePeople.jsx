5ad0c05bdb23f82bcf9311171a77a93df3fc0e5e
Joao Padilha <jvlpadilha@outlook.com>
2023-08-23 16:09:54 -0300

    feat: infinite scroll

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
@@ -7,7 +8,35 @@ const fetchUrl = async (url) => {
   return response.json();
 };
 
+
 export function InfinitePeople() {
+  const {data, error, isLoading, isError, isFetching, fetchNextPage, hasNextPage} = useInfiniteQuery({
+   queryKey: ['sw-key'],
+   queryFn: ({pageParam = initialUrl}) => fetchUrl(pageParam),
+   getNextPageParam: (lastPage) => lastPage.next || undefined
+  })
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  if(isError) return <p>error</p>
+  if(isLoading) return <h3>loading</h3>
+  return(
+    <>
+    {isFetching && <h3 className="loading">loading</h3>}
+     <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {data.pages.map(pageData => {
+        return (
+          pageData.results.map(person => {
+            return (
+              <Person
+              key={person.name}
+              name={person.name}
+              hairColor={person.hair_color}
+              eyeColor={person.eyeColor}
+              />
+            )
+          })
+        )
+      })}
+      </InfiniteScroll>
+     </>
+     )
 }
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const initialUrl = "https://swapi.dev/api/species/";
 const fetchUrl = async (url) => {
@@ -9,5 +10,39 @@ const fetchUrl = async (url) => {
 
 export function InfiniteSpecies() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    isLoading,
+    isError,
+    hasNextPage,
+    isFetchingNextPage,
+    isFetching,
+    fetchNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["species"],
+    queryFn: ({pageParam = initialUrl}) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => lastPage.next || undefined,
+  });
+
+  if (isLoading) return <p>Loading</p>;
+  if (isError) return <p>Error</p>;
+  return (
+    <>
+      {isFetching && <h2 className="loading">Loading</h2>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((specie) => {
+          return specie.results.map((pageData) => {
+            return (
+              <Species
+                key={pageData.name}
+                name={pageData.name}
+                language={pageData.language} 
+                averageLifespan={pageData.average_lifespan}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
